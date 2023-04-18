cd08b0e2c076c748531c7f0d4af9c5a0f4e36b8e
Victor Severin <severinv@list.ru>
2023-04-18 09:27:55 +0300

    completed

--- src/main/java/com/example/homework29/services/DepartmentService.java
+++ src/main/java/com/example/homework29/services/DepartmentService.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
-
+//d
 @Service
 public class DepartmentService {
     private List<Employee> staff = new ArrayList<>(List.of(
